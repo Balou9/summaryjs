1fce0556e019a27ef7d7edf15c6e00a4f6a43251
Balou9 <emmanuel.owosekun@gmail.com>
2018-05-03 00:04:57 +0200

    num

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 function summary (arr) {
   var N = arr.length
-  // better opts
-  function quartiles (opt, arr) {
-    var index = 0
-    if (opt === 'first') {
-      index = N * 0.25
-    }
-    if (opt === 'third') {
-      index =  N * 0.75
-    }
+  function quartiles (arr, opt) {
+    var index = N * opt
     if (index % 1 !== 0) {
       return (arr[index - 0.5] + arr[index + 0.5]) / 2
     }
@@ -16,34 +9,30 @@ function summary (arr) {
       return arr[index]
     }
   }
-
   function median (arr) {
     if (N % 2 === 0){
-      var med = 1/2 * (arr[N/2] + arr[N/2+1])
+      var med = 1/2 * (arr[N / 2] + arr[N / 2 + 1])
     }
     if (N % 2 === 1){
       var med = arr[(N + 1)/2]
     }
     return med
   }
-
   function sum (arr) {
     result = 0
-    for (var i = 0; i < N; i++){
+    for (var i = 0; i < N; i++) {
       result += arr[i]
     }
     return result
   }
-
-  arr.sort(function (a,b){
+  arr.sort( function (a,b) {
     return a - b
   })
-
   return {
     'Min': Math.min(...arr),
-    '1st Quartile': quartiles('first', arr),
+    '1st Quartile': quartiles(arr, 0.25),
     'Median': median(arr),
-    '3rd Quartile': quartiles('third', arr),
+    '3rd Quartile': quartiles(arr, 0.75),
     'Max': Math.max(...arr),
     'Sum': sum(arr)
   }
